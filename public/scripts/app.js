759b3e62cf041fd79332bfc2cd35b69fcd4f7fc6
krasniqir <ron.krasniqi@outlook.com>
2018-09-25 17:29:42 +0100

    initial render

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,4 +3,4 @@ console.log('App.js is running')
 var template = <p>This is JSX from app.js!</p>;
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, )
+ReactDOM.render(template, appRoot)
